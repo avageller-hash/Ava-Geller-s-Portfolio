c10205fdca3a303faa4d5f4e2314285f420574a9
Ava Geller <avageller@g.ucla.edu>
2026-03-01 12:15:53 -0800

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -29,10 +29,11 @@ export const entryAnimation = {
  * Including one folder icon 'PROJEKTY' for a realistic desktop feel.
  */
 export const DESKTOP_ICONS: DesktopIconData[] = [
-  { id: 'ramowka', title: 'SNEEX CAMPAIGN', iconSrc: 'https://i.imgur.com/ao97oFf.jpeg', x: 32, y: 34, orientation: 'vertical', type: 'file', isProminent: true },
-  { id: 'sneex-editorial', title: 'Sneex Editorial', iconSrc: 'https://i.imgur.com/zjtRYmQ.jpeg', x: 51, y: 36, orientation: 'vertical', type: 'file' },
-  { id: 'love', title: 'STYLE BUNDLES BUSINESS', iconSrc: 'https://i.imgur.com/AzV3OrU.png', x: 70, y: 33, orientation: 'horizontal', type: 'file' },
-  { id: 'projekty', title: 'Creative video editing', iconSrc: '', x: 29, y: 62, type: 'folder' },
-  { id: 'event-planner', title: 'Ava the Event Planner', iconSrc: 'https://i.imgur.com/2wICCyn.jpeg', x: 48, y: 59, orientation: 'vertical', type: 'file' },
-  { id: 'szept', title: 'THE 04 BRAND', iconSrc: 'https://i.imgur.com/pcfc0Mh.jpeg', x: 69, y: 64, orientation: 'horizontal', type: 'file' },
+  { id: 'ramowka', title: 'SNEEX CAMPAIGN', iconSrc: 'https://i.imgur.com/ao97oFf.jpeg', x: 31, y: 29, orientation: 'vertical', type: 'file', isProminent: true },
+  { id: 'sneex-editorial', title: 'Sneex Editorial', iconSrc: 'https://i.imgur.com/zjtRYmQ.jpeg', x: 52, y: 31, orientation: 'vertical', type: 'file' },
+  { id: 'love', title: 'STYLE BUNDLES BUSINESS', iconSrc: 'https://i.imgur.com/AzV3OrU.png', x: 71, y: 28, orientation: 'horizontal', type: 'file' },
+  { id: 'nyc-mission', title: '48 Hours, 115 Strangers, and One Billionaire Boss', iconSrc: 'https://i.imgur.com/yAOeIiF.png', x: 42, y: 47, orientation: 'vertical', type: 'file' },
+  { id: 'projekty', title: 'Creative video editing', iconSrc: '', x: 61, y: 44, type: 'folder' },
+  { id: 'event-planner', title: 'Ava the Event Planner', iconSrc: 'https://i.imgur.com/2wICCyn.jpeg', x: 29, y: 63, orientation: 'vertical', type: 'file' },
+  { id: 'szept', title: 'THE 04 BRAND', iconSrc: 'https://i.imgur.com/pcfc0Mh.jpeg', x: 51, y: 61, orientation: 'horizontal', type: 'file' },
 ];
